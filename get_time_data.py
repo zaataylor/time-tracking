79ce7e7a39fac2da496b19a62fcfb22cd80d4452
Zachary A. Taylor <ztaylor45685@gmail.com>
2020-11-29 14:35:14 -0500

    Added documentation for all functions

--- get_time_data.py
+++ get_time_data.py
@@ -2,22 +2,46 @@ import os
 import time
 import json
 import math
-from typing import Dict, Tuple, List
+from typing import Dict, Tuple, List, Any
 
 import requests
 
 BASE_ENDPOINT = 'https://api.clockify.me/api/v1/'
 USER_ENDPOINT = BASE_ENDPOINT + 'user'
+
 API_KEY = os.getenv('CLOCKIFY_API_KEY')
 if API_KEY is None:
     exit('Error: API key is None')
+# required header for all API requests
 X_API_HEADER = {'X-Api-Key': API_KEY}
-DEFAULT_ENTRIES_PER_PAGE = 50
 
-# Get information for currently logged in user GET /user
-
+# Check https://clockify.me/developers-api, in the section
+# about time entry request parameters
+ENTRIES_PER_PAGE = 50
 
 def get_user_info() -> Tuple:
+    """Get user ID and workspace ID for currently logged in user.
+    
+    This function returns the user ID and workspace ID for
+    the currently logged in Clockify user.
+
+    Parameters
+    ----------
+    `None`
+
+    Returns
+    -------
+    `Tuple`\n
+    A tuple `t` such that `t[0]` is the user ID and `t[1]`\n
+    is the workspace ID
+
+    Raises
+    ------
+    `KeyError`\n
+    If the response from the Clockify API does not contain\n
+    fields for 'id' and 'activeWorkspace', corresponding to the\n
+    user ID and workspace ID, respectively.
+    """
     r = requests.get(USER_ENDPOINT, headers=X_API_HEADER)
     response = r.json()
     user_id = workspace_id = None
@@ -28,11 +52,31 @@ def get_user_info() -> Tuple:
     except KeyError:
         raise
 
-# Get task IDs and names
-# GET /workspaces/{workspaceId}/projects/{projectId}/tasks
-
-
 def get_tasks_by_project_id(workspace_id: str, project_id: str) -> Dict:
+    """Get tasks associated with a given project ID and workspace ID.
+    
+    This function returns a dictionary of tasks corresponding to a given
+    project ID and workspace ID.
+    
+    The dictionary has task IDs as keys and task names as values.
+    
+    Parameters
+    -----------
+    `workspace_id`: `str`\n
+    The ID of the workspace the project is located in.
+
+    `project_id`: `str`\n
+    The ID of the project in the workspace
+
+    Returns
+    -------
+    `Dict`\n
+    A dictionary, `tasks`, as described above.
+
+    Raises
+    -------
+    `None`
+    """
     tasks = {}
     url = BASE_ENDPOINT + '/workspaces/{}/projects/{}/tasks'.format(
         workspace_id, project_id)
@@ -42,11 +86,42 @@ def get_tasks_by_project_id(workspace_id: str, project_id: str) -> Dict:
         tasks[task['id']] = task['name']
     return tasks
 
-# Get project IDs with names and associated tasks
-# GET /workspaces/{workspaceId}/projects
-
-
 def get_projects(workspace_id: str, delay: float = 0.2) -> Dict:
+    """Get project names, IDs, and associated tasks.
+    
+    The function returns information about the projects associated
+    with a particular workspace in the form of a dictionary.
+
+    For a given key `k` in the returned `projects` dictionary,
+    `k` is the project ID, and the value at `projects[k]` is a
+    list `v` such that `v[0]` is the project name associated
+    with `k`, and `v[1]` is a dictionary of tasks for the
+    particular project. This dictionary has task IDs as keys
+    and task names as values.
+
+    Thus, this dictionary retains the hierarchical structure
+    of projects and tasks as they appear in Clockify.
+
+    Parameters
+    ----------
+    `workspace_id`: `str`\n
+    The ID of the workspace to get project information from.
+
+    `delay`: `float`\n
+    An optional parameter indicating the delay between requests\n
+    made to the Clockify API. The default is 0.2 seconds to respect\n
+    Clockify's rate limiting of 10 requests/second\n
+    (https://clockify.me/developers-api).
+
+    Returns
+    -------
+    `Dict`\n
+    A dictionary matching the specifications described above.
+
+    Raises
+    ------
+    `None`
+    """
     projects = {}
     url = BASE_ENDPOINT + '/workspaces/{}/projects'.format(workspace_id)
     r = requests.get(url, headers=X_API_HEADER)
@@ -59,16 +134,58 @@ def get_projects(workspace_id: str, delay: float = 0.2) -> Dict:
         time.sleep(delay)
     return projects
 
-# Get time entries
-# GET /workspaces/{workspaceId}/user/{userId}/time-entries
-
-
 def get_time_entries(workspace_id: str, user_id: str,
                      maxpages: int, num_entries: int = 0, delay: float = 0.2) -> List:
+    """Get the time entries for a specific user in a specific workspace.
+    
+    This function returns a list of some (or all) of the time entries
+    corresponding to a specific user ID and a specific workspace ID.
+
+    Currently, Clockify does not have an API for GETting the total number of
+    entries created by a particular user, but this information is displayed at
+    the bottom of the time tracker UI. If you want to get all of the entries
+    corresponding to a given user, you can pass in an arbitrarily high
+    value for `maxpages` (not recommended) or get the number of entries from
+    looking at the UI and pass this in as `num_entries`, then use a dummy value
+    for `maxpages`. Another alternative is to get the number of entries, `x`,
+    from the UI, then let `maxpages` equal `int(math.ceil(x / ENTRIES_PER_PAGE))`,
+    where `ENTRIES_PER_PAGE` is the default number of entries per page returned
+    by the Clockify API (currently 50).
+
+    Parameters
+    ----------
+    `workspace_id`: `str`\n
+    The ID of the workspace to pull time entries from.
+
+    `user_id`: `str`\n
+    The ID of the user in the workspace to pull time entries from.
+
+    `maxpages`: `int`\n
+    The maximum number of contiguous pages to request from the API.
+
+    `num_entries`: `int`
+    An optional parameter indicating the number of entries, and thus, the\n
+    number of pages, to request from the API. Explanation: Since the\n
+    default number of entries per page is 50, per \n
+    (https://clockify.me/developers-api), the value\n
+    that `num_entries` takes on is really a proxy for how many pages to\n
+    request. If `num_entries` is specified, the number of pages is\n
+    calculated by taking `math.ceil(num_entries / 50)`.
+
+    `delay`: `float`\n
+    An optional parameter indicating the delay between requests\n
+    made to the Clockify API. The default is 0.2 seconds to respect\n
+    Clockify's rate limiting of 10 requests/second\n
+    (https://clockify.me/developers-api).
+
+    Returns
+    -------
+    `List`\n
+    A list of time entries, each of which is a `dict` with fields\n
+    matching those described in the API documentation.
+    """
     if num_entries != 0:
-        # set maxpages based on the default number of results returned per page,
-        # which is currently 50 (https://clockify.me/developers-api)
-        maxpages = int(math.ceil(num_entries / DEFAULT_ENTRIES_PER_PAGE))
+        maxpages = int(math.ceil(num_entries / ENTRIES_PER_PAGE))
 
     url = BASE_ENDPOINT + \
         '/workspaces/{}/user/{}/time-entries'.format(workspace_id, user_id)
@@ -82,8 +199,29 @@ def get_time_entries(workspace_id: str, user_id: str,
         time.sleep(delay)
     return entries
 
-
-def dump_data(data, filename: str) -> None:
+def dump_data(data: Any, filename: str) -> None:
+    """Dump data to a given filename.
+    
+    This function uses the `json.dump` function to dump
+    data to a given filename. In this context, data will
+    be either of `Dict` or `List` type.
+    
+    Parameters
+    -----------
+    `data`: `Any`\n
+    The data to dump.
+    
+    `filename`: `str`\n
+    The name of the file to dump the data to.
+    
+    Returns
+    --------
+    `None`
+    
+    Raises
+    ------
+    `None`
+    """
     d = open(filename, mode='w')
     json.dump(data, d)
     d.close()
